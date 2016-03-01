69a36e39e08f46b2bd708d265792a5837f90a74b
pako1337 <pako1337@gmail.com>
2016-03-01 19:30:08 +0100

    initial project with nancy

---
+++ PaCode.Raim/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Owin;
+
+namespace PaCode.Raim
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseNancy();
+        }
+    }
+}
