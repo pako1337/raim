6be50f6092aa6b00eb9db99c44adeb0d239ed815
pako1337 <pako1337@gmail.com>
2016-03-14 18:15:37 +0100

    iife for main raim code

--- PaCode.Raim/Content/Raim.js
+++ PaCode.Raim/Content/Raim.js
@@ -1,11 +1,13 @@
-﻿var raim = $.connection.raimHub;
-raim.client.registered = function (who) {
-    var players = document.getElementById('playersList');
-    var playerNameElement = document.createElement('span');
-    playerNameElement.textContent = who;
-    players.appendChild(playerNameElement);
-};
+﻿(function () {
+    var raim = $.connection.raimHub;
+    raim.client.registered = function (who) {
+        var players = document.getElementById('playersList');
+        var playerNameElement = document.createElement('span');
+        playerNameElement.textContent = who;
+        players.appendChild(playerNameElement);
+    };
 
-$.connection.hub.start().done(function () {
-    raim.server.register('<img src=x onerror="alert(1)"></script>');
-});
+    $.connection.hub.start().done(function () {
+        raim.server.register("Pako");
+    });
+})();
