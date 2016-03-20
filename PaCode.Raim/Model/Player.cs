2ff759b6add46e2b32804f318add817113a13377
pako1337 <pako1337@gmail.com>
2016-03-20 18:16:55 +0100

    extracted vector class to separate file

--- PaCode.Raim/Model/Player.cs
+++ PaCode.Raim/Model/Player.cs
@@ -20,16 +20,4 @@ namespace PaCode.Raim.Model
             };
         }
     }
-
-    public class Vector2d
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public Vector2d(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
 }
---
+++ PaCode.Raim/Model/Vector2d.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaCode.Raim.Model
+{
+    public class Vector2d
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector2d(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
