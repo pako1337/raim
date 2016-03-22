50d106b214ffdd64bb7d919d7be55872a9859856
pako1337 <pako1337@gmail.com>
2016-03-22 19:59:30 +0100

    hold internal players list and expose it to outside

--- PaCode.Raim/Content/PlayersList.js
+++ PaCode.Raim/Content/PlayersList.js
@@ -1,15 +1,28 @@
 ﻿function PlayersList(args) {
     args = args || {};
     var playersListElementId = args.playersList || 'playersList';
+    var _players = [];
 
     var addNewPlayer = function (player) {
+        _players.push(player);
+
         var players = document.getElementById(playersListElementId);
         var playerNameElement = document.createElement('span');
         playerNameElement.textContent = player.Name;
         players.appendChild(playerNameElement);
     };
 
+    var count = function () {
+        return _players.length;
+    };
+
+    var get = function (i) {
+        return _players[i];
+    };
+
     return {
-        addNewPlayer: addNewPlayer
+        addNewPlayer: addNewPlayer,
+        count: count,
+        get: get
     };
 }
