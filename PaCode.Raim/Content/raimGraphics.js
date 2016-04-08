bbf6657bd9c32b7da662142a779ab7fc4344ea0f
pako1337 <pako1337@gmail.com>
2016-04-08 19:08:41 +0200

    use viewport to define visible area

--- PaCode.Raim/Content/raimGraphics.js
+++ PaCode.Raim/Content/raimGraphics.js
@@ -2,6 +2,7 @@
     var canvas = args.canvas;
     var objects = args.objects;
     var drawingContext = canvas.getContext("2d");
+    var viewport = { x: 0, y: canvas.height };
 
     var lastFrameTime;
     
@@ -18,35 +19,45 @@
 
             gameObject.Position.X += gameObject.Speed.X * timeDiff;
             gameObject.Position.Y += gameObject.Speed.Y * timeDiff;
-
-            drawingContext.beginPath();
-
-            drawingContext.strokeStyle = "#F00";
-            drawingContext.fillStyle = "#F00";
-            drawingContext.arc(gameObject.Position.X, -gameObject.Position.Y, gameObject.Size, 0, 2 * Math.PI);
-            drawingContext.stroke();
-            drawingContext.fill();
-
-            drawingContext.closePath();
-
-            drawingContext.beginPath();
-
-            drawingContext.strokeStyle = "#0F0";
-            
-            var x = gameObject.Position.X + gameObject.FacingDirection.X * gameObject.Size / 2;
-            var y = gameObject.Position.Y + gameObject.FacingDirection.Y * gameObject.Size / 2;
-            drawingContext.moveTo(x, -y);
-
-            x = gameObject.Position.X + gameObject.FacingDirection.X * gameObject.Size;
-            y = gameObject.Position.Y + gameObject.FacingDirection.Y * gameObject.Size;
-            drawingContext.lineTo(x, -y);
-            drawingContext.stroke();
-
+            drawObject(gameObject);
         }
 
         lastFrameTime = timestamp;
         requestAnimationFrame(drawArena);
     };
+
+    function drawObject(gameObject) {
+        var x, y;
+        drawingContext.beginPath();
+
+        drawingContext.strokeStyle = "#F00";
+        drawingContext.fillStyle = "#F00";
+
+        x = gameObject.Position.X - viewport.x;
+        y = gameObject.Position.Y - viewport.y;
+        drawingContext.arc(x, -y, gameObject.Size, 0, 2 * Math.PI);
+        drawingContext.stroke();
+        drawingContext.fill();
+
+        drawingContext.closePath();
+
+        drawingContext.beginPath();
+
+        drawingContext.strokeStyle = "#0F0";
+
+        x = gameObject.Position.X + gameObject.FacingDirection.X * gameObject.Size / 2;
+        y = gameObject.Position.Y + gameObject.FacingDirection.Y * gameObject.Size / 2;
+        x -= viewport.x;
+        y -= viewport.y;
+        drawingContext.moveTo(x, -y);
+
+        x = gameObject.Position.X + gameObject.FacingDirection.X * gameObject.Size;
+        y = gameObject.Position.Y + gameObject.FacingDirection.Y * gameObject.Size;
+        x -= viewport.x;
+        y -= viewport.y;
+        drawingContext.lineTo(x, -y);
+        drawingContext.stroke();
+    }
 
     var startRendering = function () {
         requestAnimationFrame(drawArena);
@@ -54,5 +65,6 @@
 
     return {
         startRendering: startRendering,
+        viewport: viewport
     };
 };
