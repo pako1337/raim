1099a19cb516cc9e5c52bd256ad1dd051ff046f9
pako1337 <pako1337@gmail.com>
2016-07-01 19:24:17 +0200

    IE support for find function

---
+++ PaCode.Raim/Scripts/helpers.js
@@ -0,0 +1,8 @@
+﻿if (!Array.prototype.find) {
+    Array.prototype.find = function (predicate) {
+        for (var i = 0; i < this.length; i++) {
+            if (predicate(this[i]))
+                return this[i];
+        }
+    }
+}
