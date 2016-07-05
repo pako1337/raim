67c364d84e64f6ee8b0ecac3db7f44f9586f2744
pako1337 <pako1337@gmail.com>
2016-07-05 21:00:08 +0200

    map generator can read maps from file

--- PaCode.Raim/Model/MapGenerator.cs
+++ PaCode.Raim/Model/MapGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace PaCode.Raim.Model
@@ -8,6 +10,65 @@ namespace PaCode.Raim.Model
     public class MapGenerator
     {
         private readonly Random rnd = new Random();
+        private Regex _commentRegex = new Regex("--.*", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        public IEnumerable<Obstacle> FromFile()
+        {
+            var obstacles = new List<Obstacle>(10);
+            Vector2d arenaSize = null;
+
+            using (var reader = new StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "ArenaDefinitions/Arena51.txt")))
+            {
+                var size = reader.ReadLine()
+                                 .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(s => double.Parse(s))
+                                 .ToArray();
+                arenaSize = new Vector2d(size[0], size[1]);
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = _commentRegex.Replace(line, "");
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    var obstaclePoints = line.Split(',')
+                                             .Select(s => s.Trim())
+                                             .Select((s, i) => new { s, index = i / 2 })
+                                             .GroupBy(s => s.index)
+                                             .Select(s => new[] { double.Parse(s.First().s), double.Parse(s.Skip(1).First().s) })
+                                             .Select(s => new Vector2d(s[0], s[1]))
+                                             .ToArray();
+                    obstacles.Add(new Obstacle(obstaclePoints));
+                }
+            }
+
+            int borderMargin = 700;
+            obstacles.AddRange(new List<Obstacle>() {
+                new Obstacle( // top
+                    new Vector2d(-borderMargin, arenaSize.Y),
+                    new Vector2d(-borderMargin, arenaSize.Y + borderMargin),
+                    new Vector2d(arenaSize.X + borderMargin, arenaSize.Y + borderMargin),
+                    new Vector2d(arenaSize.X + borderMargin, arenaSize.Y)),
+                new Obstacle( // right
+                    new Vector2d(arenaSize.X, 0),
+                    new Vector2d(arenaSize.X, arenaSize.Y),
+                    new Vector2d(arenaSize.X + borderMargin, arenaSize.Y),
+                    new Vector2d(arenaSize.X + borderMargin, 0)),
+                new Obstacle( // bottom
+                    new Vector2d(-borderMargin, 0),
+                    new Vector2d(arenaSize.X + borderMargin, 0),
+                    new Vector2d(arenaSize.X + borderMargin, -borderMargin),
+                    new Vector2d(-borderMargin, -borderMargin)),
+                new Obstacle( // left
+                    new Vector2d(0, 0),
+                    new Vector2d(-borderMargin, 0),
+                    new Vector2d(-borderMargin, arenaSize.Y),
+                    new Vector2d(0, arenaSize.Y))
+            });
+
+            return obstacles;
+        }
 
         public IEnumerable<Obstacle> Generate(Vector2d arenaSize)
         {
