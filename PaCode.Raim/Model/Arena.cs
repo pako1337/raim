d9d951e540e4d30d5fb96a2d30752865524d77eb
pako1337 <pako1337@gmail.com>
2016-07-11 17:24:02 +0200

    spin lock for synchronization

--- PaCode.Raim/Model/Arena.cs
+++ PaCode.Raim/Model/Arena.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using PaCode.Raim.Home;
 
 namespace PaCode.Raim.Model
@@ -10,7 +11,7 @@ namespace PaCode.Raim.Model
     public class Arena
     {
         private static Random rnd = new Random();
-        private static object _lock = new object();
+        private SpinLock _lock = new SpinLock();
         private CollisionEngine _collisionEngine;
         private Vector2d _arenaSize;
         private const int MaxTimeBetweenFrames = 1000 / 20;
@@ -29,17 +30,35 @@ namespace PaCode.Raim.Model
         public Player RegisterPlayer(string name)
         {
             var player = Player.Create(name, rnd.NextDouble() * ArenaSize.X, rnd.NextDouble() * ArenaSize.Y);
-            lock (_lock)
+            bool lockTaken = false;
+            try
+            {
+                _lock.Enter(ref lockTaken);
                 GameObjects.Add(player);
-            return player;
+                return player;
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
+            }
+
+            return null;
         }
 
         public void UnregisterPlayer(Player player)
         {
-            lock (_lock)
+            bool lockTaken = false;
+            try
             {
+                _lock.Enter(ref lockTaken);
                 GameObjects.RemoveAll(g => g.Id == player.Id);
                 GameObjects.RemoveAll(b => b is Bullet && ((Bullet)b).Player.Id == player.Id);
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
             }
         }
 
@@ -52,8 +71,10 @@ namespace PaCode.Raim.Model
             if (timeBetweenEvents > MaxTimeBetweenFrames)
                 timeBetweenEvents = MaxTimeBetweenFrames;
 
-            lock (_lock)
+            bool lockTaken = false;
+            try
             {
+                _lock.Enter(ref lockTaken);
                 foreach (var gameObject in GameObjects)
                 {
                     gameObject.Position = gameObject.Position.Add(gameObject.Speed.Scale(timeBetweenEvents / 1000));
@@ -71,6 +92,11 @@ namespace PaCode.Raim.Model
 
                 return GameObjects.ToArray();
             }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
+            }
         }
 
         public IEnumerable<IGameObject> RemoveDestroyedObjects()
@@ -83,10 +109,17 @@ namespace PaCode.Raim.Model
 
         public void ProcessInput(PlayerInput input, Player player)
         {
-            lock (_lock)
+            bool lockTaken = false;
+            try
             {
+                _lock.Enter(ref lockTaken);
                 var createdObjects = player.ProcessInput(input, DateTime.Now);
                 GameObjects.AddRange(createdObjects);
+            }
+            finally
+            {
+                if (lockTaken)
+                    _lock.Exit();
             }
         }
     }
